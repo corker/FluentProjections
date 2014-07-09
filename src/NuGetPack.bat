04a511d2042819eaab9397a203b35c4d42332223
corker <corker@inbox.ru>
2014-07-09 12:24:39 +0200

    nuget packager extended with automapper package

--- src/NuGetPack.bat
+++ src/NuGetPack.bat
@@ -1,3 +1,4 @@
 .nuget\NuGet Update -self
 .nuget\NuGet pack -sym FluentProjections\FluentProjections.csproj
 .nuget\NuGet pack -sym FluentProjections.Dapper\FluentProjections.Dapper.csproj
+.nuget\NuGet pack -sym FluentProjections.AutoMapper\FluentProjections.AutoMapper.csproj
